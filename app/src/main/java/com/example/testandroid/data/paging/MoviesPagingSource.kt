61ceaa55e76dac426d1efa98fc149dcc355a7143
AsaelTF <asael_tunfranco@hotmail.com>
2023-03-14 14:42:01 -0500

    popular

--- app/src/main/java/com/example/testandroid/data/paging/MoviesPagingSource.kt
+++ app/src/main/java/com/example/testandroid/data/paging/MoviesPagingSource.kt
@@ -2,19 +2,22 @@ package com.example.testandroid.data.paging
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import com.example.testandroid.data.entities.MovieEntity
 import retrofit2.Response
 import com.example.testandroid.data.model.GetMoviesResponse
 import com.example.testandroid.data.model.Movie
+import com.example.testandroid.data.model.ResourceStatus
 import com.example.testandroid.data.repository.MovieRepository
+import com.example.testandroid.utils.Resource
 import okhttp3.internal.wait
 import retrofit2.HttpException
 
 class MoviesPagingSource(
-    private val repository: MovieRepository,)
+    private val repository: MovieRepository)
     : PagingSource<Int, Movie>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
-        TODO("prueba")
+        TODO()
     }
 
 
